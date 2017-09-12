a4589eab2da4e819843abd972fe7a5795fdea189
Elliott Lin <elliott1230@gmail.com>
2017-09-12 16:17:30 +0800

    Second update for course

---
+++ lect_11/app.js
@@ -0,0 +1,21 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.name = "Yaakov";
+
+  $scope.sayMessage = function() {
+    return "Yaakov likes to eat healthy snacks at night!";
+  }
+
+  $scope.stateOfBeing = 'hungry';
+  $scope.feedYaakov = function(){
+    $scope.stateOfBeing = 'fed';
+  };
+}
+
+})();
